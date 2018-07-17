12fe0b2387c94e162ac422aceb0bec539782a571
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-07-17 14:05:54 -0400

    optimizations to remove thrashing

--- src/main/java/testopenie/testopenie/App.java
+++ src/main/java/testopenie/testopenie/App.java
@@ -22,27 +22,33 @@ import org.lambda3.graphene.core.relation_extraction.model.ExContent;
 public class App {
 	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 		int numThreads = 96;
-		
+
 		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-		LinkedBlockingQueue<String> outs = new LinkedBlockingQueue<String>();
-		LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<String>();
-		try (BufferedReader br = new BufferedReader(new FileReader(new File("./test.txt")))) {
-		    String line;
-		    while ((line = br.readLine()) != null) {
-		       queue.put(line.trim());
-		    }
-		}
-		long initialSize = queue.size();
-		
+		LinkedBlockingQueue[] queues = new LinkedBlockingQueue[numThreads];
+
 		for (int i = 0; i < numThreads; i++) {
-			executor.submit(new GrapheneWorker(queue, outs));
+			queues[i] = new LinkedBlockingQueue<String>();
 		}
-		
-		while (outs.size() < initialSize) { // task is not finished
+
+		LinkedBlockingQueue<String> outs = new LinkedBlockingQueue<String>();
+		long count = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(new File("./test.txt")))) {
+			String line;
+			
+			while ((line = br.readLine()) != null) {
+				queues[(int) (++count % numThreads)].put(line.trim());
+			}
+		}
+
+		for (int i = 0; i < numThreads; i++) {
+			executor.submit(new GrapheneWorker(queues[i], outs));
+		}
+
+		while (outs.size() < count) { // task is not finished
 			System.out.println("\n--------------\n\n\nSentences done: " + outs.size() + "\n------------\n");
 			Thread.sleep(1000);
 		}
-		
+
 		executor.shutdown();
 		FileWriter writer = new FileWriter("./all_news_tgt.txt");
 		PrintWriter printWriter = new PrintWriter(writer);
--- src/main/java/testopenie/testopenie/GrapheneWorker.java
+++ src/main/java/testopenie/testopenie/GrapheneWorker.java
@@ -11,11 +11,13 @@ public class GrapheneWorker implements Runnable {
 	Graphene graphene;
 	LinkedBlockingQueue<String> queue;
 	LinkedBlockingQueue<String> outQueue;
+	LinkedBlockingQueue<String> tempOut;
 	
 	public GrapheneWorker(LinkedBlockingQueue<String> queue, LinkedBlockingQueue<String> outQueue) {
 		this.graphene = new Graphene();
 		this.queue = queue;
 		this.outQueue = outQueue;
+		this.tempOut = new LinkedBlockingQueue<String>();
 	}
 	
 	@Override
@@ -24,7 +26,10 @@ public class GrapheneWorker implements Runnable {
 			String sentence = queue.remove();
 			try {
 				String json = this.graphene.doRelationExtraction(sentence, false, false).serializeToJSON();
-				outQueue.add(sentence + "\t" + json);
+				tempOut.add(sentence + "\t" + json);
+				if (this.queue.isEmpty() || (tempOut.size() > 20)){
+					tempOut.drainTo(outQueue);
+				}
 			} catch (JsonProcessingException e) {
 				e.printStackTrace();
 			}
